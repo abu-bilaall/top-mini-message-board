e2060dc366130749c2908268708111171834f3ef
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2025-04-12 19:17:45 +0100

    feat: update controllers to use db queries

--- controllers/createNewMessage.js
+++ controllers/createNewMessage.js
@@ -1,16 +1,16 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const asyncHandler = require("express-async-handler");
-const messages = require("../models/messages");
+const db = require("../models/queries");
 const ClientSideError = require('../errors/ClientSideError');
 
-const addNewMessage = asyncHandler((req, res) => {
+const addNewMessage = asyncHandler(async (req, res) => {
   const { message, author } = req.body;
 
   if (!message || !author) {
     throw new ClientSideError('Author or Message not specified.');
   }
 
-  messages.push({ text: message, user: author, added: new Date() });
+  await db.addNewEntry(author, message, new Date());
   res.redirect("/");
 });
 
--- controllers/getIndexPage.js
+++ controllers/getIndexPage.js
@@ -1,11 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const asyncHandler = require("express-async-handler");
-const messages = require("../models/messages");
+const db = require("../models/queries");
 
-const newMessageEndpoint = "/new";
-
-const getIndexPage = asyncHandler((req, res) => {
-  res.render("index", { title: "Mini Messageboard", messages, newMessageEndpoint });
+const getIndexPage = asyncHandler(async (req, res) => {
+  const dbMessages = await db.getUsers();
+  res.render("index", { title: "Mini Messageboard", dbMessages });
 });
 
 module.exports = getIndexPage;
